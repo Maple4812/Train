fe13f3746fd3280b5da85623452ae2dc3a01f841
Dambi4812 <skagusgh113@gmail.com>
2024-04-12 21:22:39 +0900

    Station 클래스 생성

---
+++ src/Station.java
@@ -0,0 +1,31 @@
+import java.util.regex.Pattern;
+
+public class Station {
+    private static final String REGEXP_PATTERN_NORMAL = "[가-핳]역"; //xx역 형식
+    private static final String REGEXP_PATTERN_WITHOUT = "[가-핳]"; //xx 형식
+    private static final String REGEXP_PATTERN_DOUBLE = "[가-핳]역역"; //xx역역 형식 (방지하기 위함)
+    String station;
+
+    public Station(String str) throws FileIntegrityException {
+        checkIntegrity(str);
+        if(str.matches(REGEXP_PATTERN_WITHOUT)){
+            str += "역";  //xx에 '역' 추가
+            this.station = str;
+        }
+        else{
+            this.station = str;
+        }
+    }
+
+    public static void checkIntegrity(String str) throws FileIntegrityException {
+        if(!Pattern.matches(REGEXP_PATTERN_NORMAL, str) || !Pattern.matches(REGEXP_PATTERN_WITHOUT, str)) {
+            if(Pattern.matches(REGEXP_PATTERN_DOUBLE, str)) {
+                throw new FileIntegrityException("역명 무결성 오류");
+            }
+        }
+    }
+
+    public String getStation() {
+        return station;
+    }
+}
