d058b0cc620f0e0cc206f9cf3c68f53f99bac41c
Dambi4812 <skagusgh113@gmail.com>
2024-04-12 21:11:18 +0900

    PhoneNumber 클래스 생성 (사실 수정)

--- src/PhoneNumber.java
+++ src/PhoneNumber.java
@@ -1,24 +1,21 @@
-public class PhoneNumber {
-    String REGEXP_PATTERN_DASH = "[0-9]{3}-[0-9]{4}-[0-9]{4}";
-    String REGEXP_PATTERN_NORMAL = "[0-9]{11}";
-    String phoneNumber = "";
+import java.util.regex.Pattern;
 
-    public PhoneNumber(String input) throws Exception {
-        if(input.matches(REGEXP_PATTERN_DASH)){
-            String[] tempArr = input.split("-");
-            StringBuilder strBuilder = new StringBuilder(phoneNumber);
-            for(String s: tempArr){
-                strBuilder.append(s);
-            }
-            phoneNumber = strBuilder.toString();
-        } else if (input.matches(REGEXP_PATTERN_NORMAL)) {
-            phoneNumber = input;
-        } else {
-            throw new Exception("잘못된 전화번호 형식입니다.");
+public class PhoneNumber {
+    private static final String REGEXP_PATTERN_NORMAL = "010[0-9]{8}"; //010xxxxxxxx 형식
+    String phoneNumber;
+
+    public PhoneNumber(String str) throws FileIntegrityException {
+        checkIntegrity(str);
+        this.phoneNumber = str;
+    }
+
+    public static void checkIntegrity(String str) throws FileIntegrityException {
+        if(!Pattern.matches(REGEXP_PATTERN_NORMAL, str)) {
+            throw new FileIntegrityException("전화번호 무결성 오류");
         }
     }
 
-    public void printPhoneNumber(){
-        System.out.println(phoneNumber);
+    public String getPhoneNumber() {
+        return phoneNumber;
     }
 }
