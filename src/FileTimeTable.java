1edd218ea30e856784db236498ccf1157ce9873f
kjh08201 <kjh08201@gmail.com>
2024-04-26 15:18:11 +0900

    Changes

--- src/FileTimeTable.java
+++ src/FileTimeTable.java
@@ -1,8 +1,39 @@
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
 
 public class FileTimeTable implements FileInterface{
+    private String fileName;
+//    private FileWriter fw; // 예약 시 여석 수가 줄어드는 걸 csv 파일에 업데이트해야함
+//    private PrintWriter writer;
+    private final SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmm");
+    Scanner scan = new Scanner(new File(fileName));
+
+    public FileTimeTable(String fileName) throws FileNotFoundException {
+        this.fileName = fileName;
+    }
+
     @Override
-    public void checkIntegrity() throws FileNotFoundException {
+    public void checkIntegrity() throws FileNotFoundException, FileIntegrityException {
+        while(scan.hasNextLine()){
+            String[] strArr = scan.nextLine().split(","); //한 줄 읽어온 다음 split
+            if(strArr.length != 8) {
+                throw new FileIntegrityException("무결성 오류: 파일에 인자의 개수가 옳지 않은 레코드가 존재합니다.");
+            }
+            UserName.checkIntegrity(strArr[0]);  //노선번호 무결성 확인
+            Time.checkIntegrity(strArr[1]);  //출발 시각 무결성 확인
+            Station.checkIntegrity(strArr[2]);  //출발역 무결성 확인
+            Time.checkIntegrity(strArr[3]);  //도착 시각 무결성 확인
+            Station.checkIntegrity(strArr[4]);  //도착역 무결성 확인
+//            가격.checkIntegrity(strArr[5]);  //가격 무결성 확인, Ticket 객체에서 노선 번호와 함께 무결성검사 할 것인지, 따로 price 객체를 만들지?
+            Seat.checkIntegrity(strArr[6]);  //여석 수 무결성 확인
+            Seat.checkIntegrity(strArr[7]);  //전체 좌석 수 무결성 확인
+
+
+        }
 
     }
 }
---
+++ src/Seat.java
@@ -0,0 +1,24 @@
+import java.util.regex.Pattern;
+
+public class Seat {
+    /*
+        여석 수는 0이 될 수 있지만 전체 좌석 수는 0이 될 수 없습니다.
+        '1~999'와 '0' 두 개의 패턴으로 나누어 무결성검사를 진행합니다.
+    */
+    private static final String REGEXP_PATTERN_NORMAL="^[1-9][0-9]{0,2}$";
+    private static final String REGEXP_PATTERN_ZERO="^0$";
+    String seat;
+
+    public Seat(String seat) throws FileIntegrityException{
+        checkIntegrity(seat);
+        this.seat = seat;
+    }
+
+    public static void checkIntegrity(String str) throws FileIntegrityException {
+        if(!Pattern.matches(REGEXP_PATTERN_NORMAL, str)) {
+            if(!Pattern.matches(REGEXP_PATTERN_ZERO,str)) {
+                throw new FileIntegrityException("좌석 수 무결성 오류");
+            }
+        }
+    }
+}
