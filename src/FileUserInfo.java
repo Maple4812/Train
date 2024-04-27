0960c3bca47aeb8fa89ca1d9d04c04136ffe0fe5
lwb1112 <165344710+lwb1112@users.noreply.github.com>
2024-04-27 12:07:04 +0900

    Update FileUserInfo.java

--- src/FileUserInfo.java
+++ src/FileUserInfo.java
@@ -3,6 +3,13 @@ import java.io.*;
 import java.util.Scanner;
 
 public class FileUserInfo implements FileInterface{
+    Scanner scan;
+    String fileName;
+    FileWriter fw;
+    PrintWriter writer;
+    public FileuserInfo(String fileName) {
+        this.fileName = fileName;
+    }
     @Override
     public void checkIntegrity() throws FileNotFoundException {
 
