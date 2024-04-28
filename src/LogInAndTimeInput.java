995ad411abf589e0dedac82d41b554daab416452
lwb1112 <165344710+lwb1112@users.noreply.github.com>
2024-04-28 11:02:26 +0900

    Update LogInAndTimeInput.java

--- src/LogInAndTimeInput.java
+++ src/LogInAndTimeInput.java
@@ -66,12 +66,24 @@ public class LogInAndTimeInput {
         // TempReserve의 타이머가 꺼져있는 경우
         if (!TempReservation.isTimerOn()) {
             setNowComputerTime(getTime());
-            System.out.println("현재 시간을 입력하세요 (yyyyMMddHHmm 형식):");
-            String time = scanner.nextLine();
+            String time = inputTime(scanner);
+            if (time == null) return; // // TempReserve의 타이머가 꺼져있는 경우
             setNowTime(time);
-        } else { // TempReserve의 타이머가 켜져있는 경우
+        } else {
             setNowComputerTime(getTime());
             setNowTime(TempReservation.getNewTime());
+        }
+    }
+
+    private String inputTime(Scanner scanner) {
+        System.out.println("현재 시간을 입력하세요 (yyyyMMddHHmm 형식):");
+        String time = scanner.nextLine();
+        try {
+            Time.checkIntegrity(time); // 현재 시간의 유효성 확인
+            return time;
+        } catch (FileIntegrityException e) {
+            System.out.println("시간 형식이 올바르지 않습니다. 다시 입력해주세요.");
+            return null; // 예외 처리된 경우 null 반환
         }
     }
 
