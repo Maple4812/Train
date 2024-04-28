2068ec3034edf27c65cc57bb76a4736f5d65dc89
seonghyeonmin <139226040+seonghyeonmin@users.noreply.github.com>
2024-04-28 10:53:43 +0900

    FileUserInfo.java

--- src/FileUserInfo.java
+++ src/FileUserInfo.java
@@ -1,35 +1,32 @@
 import java.io.*;
 import java.util.Scanner;
 
-public class FileUserInfo implements FileInterface{
+public class FileUserInfo implements FileInterface {
     Scanner scan;
     String fileName;
-    FileWriter fw;
-    PrintWriter writer;
+
     public FileUserInfo(String fileName) {
         this.fileName = fileName;
     }
+
     @Override
     public void checkIntegrity() throws FileNotFoundException, FileIntegrityException {
         scan = new Scanner(new File(fileName));
-        while(scan.hasNextLine()){
-            String[] strArr = scan.nextLine().split(","); //한 줄 읽어온 다은 split
-            if(strArr.length != 2) {
+        while (scan.hasNextLine()) {
+            String[] strArr = scan.nextLine().split(","); // 한 줄 읽어온 다음 split
+            if (strArr.length != 2) {
                 throw new FileIntegrityException("무결성 오류: 파일에 인자의 개수가 옳지 않은 레코드가 존재합니다.");
             }
-            UserName.checkIntegrity(strArr[0]);  //사용자 이름 무결성 확인
-            PhoneNumber.checkIntegrity(strArr[1]);  //전화번호 무결성 확인
+            UserName.checkIntegrity(strArr[0]); // 사용자 이름 무결성 확인
+            PhoneNumber.checkIntegrity(strArr[1]); // 전화번호 무결성 확인
         }
-
     }
-    public void write(String userName, String phoneNumber) {
-        File file = new File(fileName);
-        try {
-            fw = new FileWriter(file);
-            writer = new PrintWriter(fw);
+
+    public void append(String userName, String phoneNumber) {
+        try (FileWriter fw = new FileWriter(fileName, true);
+             PrintWriter writer = new PrintWriter(fw)) {
             String str = userName + "," + phoneNumber;
             writer.println(str);
-            writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
