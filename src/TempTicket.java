6303298d6c79a49691035ca17b96d206ad97004e
jinwoo <jinub080@gmail.com>
2024-06-04 16:22:29 +0900

    add TempTicket.java and Ticket.java

--- src/TempTicket.java
+++ src/TempTicket.java
@@ -3,4 +3,28 @@ public class TempTicket extends Ticket{
     private String reserveTime;
 
     public TempTicket(){}
+
+    public String getReserveComputerTime() {
+        return reserveComputerTime;
+    }
+
+    public String getReserveTime() {
+        return reserveTime;
+    }
+
+    public int getFirstRailofTempTicket(){
+        return railIndices.get(0).railIndex;
+    }
+
+    public int getLastRailofTempTicket(){
+        return railIndices.get(railIndices.size() - 1).railIndex;
+    }
+
+    public void setReserveComputerTime(String reserveComputerTime) {
+        this.reserveComputerTime = reserveComputerTime;
+    }
+
+    public void setReserveTime(String reserveTime) {
+        this.reserveTime = reserveTime;
+    }
 }
