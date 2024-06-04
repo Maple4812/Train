4e5e2fe9feea7dcd6f0524131e8d04b7b551c7f6
kjh08201 <kjh08201@gmail.com>
2024-06-04 11:27:59 +0900

    FileTimeTable lineList 추가
    Signed-off-by: kjh08201 <kjh08201@gmail.com>

--- src/FileRail.java
+++ src/FileRail.java
@@ -6,7 +6,6 @@ public class FileRail implements FileInterface{
     private String fileName;
     private ArrayList<Rail> raillist;
     Scanner scan;
-    //저희 member 수정해야할거같아요
 
     public FileRail(String fileName) throws FileNotFoundException {
         this.fileName = fileName;
