6242ebfe9904cf391848d7735e8dd5f331816a49
kjh08201 <kjh08201@gmail.com>
2024-06-04 15:07:56 +0900

    FileRail
    Signed-off-by: kjh08201 <kjh08201@gmail.com>

--- src/FileRail.java
+++ src/FileRail.java
@@ -17,9 +17,15 @@ public class FileRail implements FileInterface{
         scan= new Scanner(new File(fileName));
         while(scan.hasNextLine()){
             String[] strArr = scan.nextLine().split(","); //한 줄 읽어온 다음 split
-            Line line=new Line(); //읽어온 줄의 정보를 저장할 Line 객체
+            Rail rail=new Rail(); //읽어온 줄의 정보를 저장할 Rail 객체
             /*
             무결성 검사 진행하는 부분 여기에 추가
+             */
+
+            /*
+            rail.rainIndex=Integer.parseInt(strArr[0]);
+            rail.fromStation=strArr........
+            이러한 형태로 rail 객체의 member들에 값을 할당
              */
         }
     }
