8ed8c790adf29e83004ad452846f005b2374d41f
DunkelH <146098704+DunkelH@users.noreply.github.com>
2024-06-05 22:59:59 +0900

    Rail.java
    생성자 수정

--- src/Rail.java
+++ src/Rail.java
@@ -4,7 +4,13 @@ public class Rail {
     String duration; // 소요 시간
     Price price; // 가격
 
-    public Rail() {}
+    public Rail(int railIndex, Station fromStation, Station toStation, String duration, Price price) {
+        this.railIndex = railIndex;
+        this.fromStation = fromStation;
+        this.toStation = toStation;
+        this.duration = duration;
+        this.price = price;
+    }
 
     /*
         1. 설계 문서에는 public static void checkIntegrity()로 되어 있고, 여기서 모든 인자와 경우를 검사한다고 되어 있음
