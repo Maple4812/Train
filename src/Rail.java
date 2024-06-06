16c03586a648fae92a0f11f888c1a6284b40635f
wookb <wookbin.lee@gmail.com>
2024-06-06 13:03:54 +0900

    Rail 클래스 무결성 검사 추가

--- src/Rail.java
+++ src/Rail.java
@@ -4,21 +4,26 @@ public class Rail {
     String duration; // 소요 시간
     Price price; // 가격
 
-    public Rail(int railIndex, Station fromStation, Station toStation, String duration, Price price) {
-        this.railIndex = railIndex;
-        this.fromStation = fromStation;
-        this.toStation = toStation;
-        this.duration = duration;
-        this.price = price;
-    }
+    public Rail() {}
 
     /*
         1. 설계 문서에는 public static void checkIntegrity()로 되어 있고, 여기서 모든 인자와 경우를 검사한다고 되어 있음
         2. 여기서 설계 문서대로 작동하려면 int 인자를 추가해 railIndex만 checkIntegrity로 검사하고 나머지는 FileRail에서 따로 검사
      */
-    public static void checkIntegrity() throws FileIntegrityException{
+    public void checkIntegrity() throws FileIntegrityException{
+        if (railIndex == 0) { //railIndex가 0인 경우
+            throw new FileIntegrityException("무결성 오류: railIndex는 0이 될 수 없습니다.");
+        }
 
+        if (fromStation.equals(toStation)){ //출발역과 도착역이 동일한 경우
+            throw new FileIntegrityException("무결성 오류: 출발역과 도착역이 동일합니다.");
+        }
 
+        //나머지 요소에 대해 무결성 검사
+        Station.checkIntegrity(fromStation.getStation());
+        Station.checkIntegrity(toStation.getStation());
+        Price.checkIntegrity(price.getPrice());
+        Time.checkIntegrity(duration);
     }
 
 
